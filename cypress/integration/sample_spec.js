c9cfb3f0280d0cbc954cb09b7d3eb4a0aa3e8d7c
Apipol Sukgler <apipol@computerlogy.com>
2018-09-29 11:04:47 +0700

    [STEP 2]: open browser and check page contains text

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,5 +1,15 @@
 describe('My First Test', () => {
     it('Does not do much', () => {
         expect(true).to.equal(true)
-    });
+    })
+
+    it('Visits the kitchen sink', () => {
+        //Only Test Apps You Control (shouldn’t test applications you don’t control)
+        //Cypress is to be a tool you use every day to build and test your own applications
+        //Cypress is not a general purpose web automation tool. 
+        //It is poorly suited for scripting live, production websites not under your control.
+        cy.visit('https://example.cypress.io')
+
+        cy.contains('type')
+    })
 });
